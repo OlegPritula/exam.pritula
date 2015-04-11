126a416d4f1784f15e6fc274b46ecd4fa62f7104
OlegPritula <oleg.pritula.76@gmail.com>
2015-04-11 15:54:01 +0300

    exam commit

---
+++ wp-content/themes/skokov/index.php
@@ -0,0 +1,376 @@
+﻿<!DOCTYPE HTML>
+<html>
+
+	<head>
+		<title>Skokov Blog</title>
+		<link href="style.css" rel="stylesheet" type="text/css" media="all" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	</head>
+
+	<body>
+
+		<div class="header">	
+			<div class="wrap"> 
+				<div class="logo">
+					<a href="index.html"><img src="images/logo.png" alt=""/></a>
+				</div>
+				<div class="cssmenu">
+					<ul>
+						<li><a href="#">Home</a></li>
+						<li><a href="#">Portfolio</a></li> 
+						<li class="active"><a href="blog.html">Blog</a></li> 
+						<li><a href="#">About Us</a></li> 
+						<li><a href="#">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+		<div class="banner">
+	      	<div class="wrap">
+	      	     <h2>Blog</h2><div class="clear"></div>
+	      	</div>
+	    </div>
+
+		<div class="main">	
+			<div class="project-wrapper">
+				<div class="project">
+				    <div class="blog-left">
+						
+						<div class="blog-quote"
+						   <h4>"Ability may get you to the top, but it takes  character to keep you there."</h4><span class="author">John Wooden</span>
+						</div>
+						<div class="blog-img">
+							<a href="single_post.html">
+							<img src="images/img_news_1.jpg" alt=""/>
+							<div class="blog-description">
+								<h5>17 Inspiring Examples of Parallax  Scrolling Sites</h5>
+								<p>When it comes to parallax scrolling, the line that separates good and bad choices is pretty thin. A while back parallax was being used to deliver an explosion of elements floating around the screen.</p>
+								<div class="comment">
+									<span class="icon"><img src="images/heart.png" title="likes" alt=""/>15</span>
+									<div class="comment-description"><a href="#">by John Doe</a> / <a href="#">4 comments</a> / <a href="#">Oct. 12, 2013</a></div>
+								</div>
+							</div>
+							</a>
+						</div>
+
+		 				<div class="blog-img">
+							<a href="single_post.html">
+							<img src="images/img_news_3.jpg" alt=""/>
+							<div class="blog-description">
+								<h5>20 Resources for Beginner Designers & Developers</h5>
+								<p>Getting started in the world of design can be both thrilling and intimidating. Sometimes, you feel like you just don’t have the tools or skills you need to complete even the most basic projects.</p>
+								<div class="comment">
+									<span class="icon"><img src="images/heart.png" title="likes" alt=""/>7</span>
+									<div class="comment-description"><a href="#">by John Doe</a> / <a href="#">8 comments</a> / <a href="#">Oct. 10, 2013</a></div>
+								</div>
+							</div>
+							</a>
+						</div>
+
+		 				<div class="blog-img">
+							<a href="single_post.html">
+							<img src="images/img_news_5.jpg" alt=""/>
+							<div class="blog-description">
+								<h5>18 Minimalistic Home Offices to  Inspire You</h5>
+								<p>It doesn’t matter if you work from home or if you use your own space only at night or on the weekends, home offices are great! They give us a chance to create a space just the way we want it.</p>
+								<div class="comment">
+									<span class="icon"><img src="images/heart.png" title="likes" alt=""/>2</span>
+									<div class="comment-description"><a href="#">by John Doe</a> / <a href="#">0 comments</a> / <a href="#">Oct. 08, 2013</a></div>
+								</div>
+							</div>
+							</a>
+						</div>
+
+					</div>
+
+					<div class="blog-right">
+
+		 				<div class="blog-img">
+							<a href="single_post.html">
+							<img src="images/img_news_2.jpg" alt=""/>
+							<div class="blog-description">
+								<h5>Multipurpose Agency WordPress  Theme</h5>
+								<p>At ThemeTrust, our premium WordPress theme shop, we just released our latest WordPress theme. Baylie is a multipurpose agency theme featuring a full screen parallax slideshow.</p>
+								<div class="comment">
+									<span class="icon"><img src="images/heart.png" title="likes" alt=""/>0</span>
+									<div class="comment-description"><a href="#">by John Doe</a> / <a href="#">1 comments</a> / <a href="#">Oct. 12, 2013</a></div>
+								</div>
+							</div>
+							</a>
+						</div>
+
+		 				<div class="blog-img">
+							<a href="single_post.html">
+							<img src="images/img_news_4.jpg" alt=""/>
+							<div class="blog-description">
+								<h5>Inspiring Examples of Color in  Web Design</h5>
+								<p>Last week we showcased 13 Beautiful Examples of Bold Colors in Web Design and we received several emails asking us to put 
+								   together a list with specific colors.</p>
+								<div class="comment">
+									<span class="icon"><img src="images/heart.png" title="likes" alt=""/>19</span>
+									<div class="comment-description"><a href="#">by John Doe</a> / <a href="#">9 comments</a> / <a href="#">Oct. 10, 2013</a></div>
+								</div>
+							</div>
+							</a>
+						</div>				
+
+		 				<div class="blog-img">
+							<a href="single_post.html">
+							<img src="images/img_news_6.jpg" alt=""/>
+							<div class="blog-description">
+								<h5>15 Fresh Examples of Clean and  Minimal Web Designs</h5>
+								<p>As a designer, it can be tempting to use a lot of unnecessary design elements or a complex color palette to make a website stand out.</p>
+								<div class="comment">
+									<span class="icon"><img src="images/heart.png" title="likes" alt=""/>21</span>
+									<div class="comment-description"><a href="#">by John Doe</a> / <a href="#">0 comments</a> / <a href="#">Oct. 09, 2013</a></div>
+								</div>
+							</div>
+							</a>
+						</div>				
+
+		 				<div class="blog-img">
+							<a href="single_post.html">
+							<img src="images/img_news_7.jpg" alt=""/>
+							<div class="blog-description">
+								<h5>18 Minimalistic Home Offices to  Inspire You</h5>
+								<p>It doesn’t matter if you work from home or if you use your own space only at night or on the weekends, home offices are great! They give us a chance to create a space just the way we want it.</p>
+								<div class="comment">
+									<span class="icon"><img src="images/heart.png" title="likes" alt=""/>2</span>
+									<div class="comment-description"><a href="#">by John Doe</a> / <a href="#">0 comments</a> / <a href="#">Oct. 8, 2013</a></div>
+								</div>
+							</div>
+							</a>
+						</div>
+
+					</div>
+				</div>
+			</div>
+
+		 	<div class="sidebar">
+				<div class="search-box">
+					<form>
+						<input type="text" value="Search...." onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search';}">
+						<input type="submit" value="">
+					</form>
+				</div>
+			
+				<div class="categories">
+			     	<h4>Categories</h4>
+					<ul class="blog-list">
+						<li><img src="images/arrow.png" alt=""><p><a href="#">web design</a></p><div class="clear"></div></li>
+						<li><img src="images/arrow.png" alt=""><p><a href="#">branding</a></p><div class="clear"></div></li>
+						<li><img src="images/arrow.png" alt=""><p><a href="#">business</a></p><div class="clear"></div></li>
+						<li><img src="images/arrow.png" alt=""><p><a href="#">inspiration</a></p><div class="clear"></div></li>
+						<li><img src="images/arrow.png" alt=""><p><a href="#">marketing</a></p><div class="clear"></div></li>
+					</ul>
+					<ul class="blog-list">
+						<li><img src="images/arrow.png" alt=""><p><a href="#">html</a></p><div class="clear"></div></li>
+						<li><img src="images/arrow.png" alt=""><p><a href="#">css</a></p><div class="clear"></div></li>
+						<li><img src="images/arrow.png" alt=""><p><a href="#">news</a></p><div class="clear"></div></li>
+						<li><img src="images/arrow.png" alt=""><p><a href="#">mobile</a></p><div class="clear"></div></li>
+						<li><img src="images/arrow.png" alt=""><p><a href="#">usability</a></p><div class="clear"></div></li>
+					</ul>
+				</div>
+
+				<div class="archive">
+			     	<h4>Archive</h4>
+					<ul class="blog-list">
+						<li><img src="images/arrow.png" alt=""><p><a href="#">web design</a></p><div class="clear"></div></li>
+						<li><img src="images/arrow.png" alt=""><p><a href="#">branding</a></p><div class="clear"></div></li>
+						<li><img src="images/arrow.png" alt=""><p><a href="#">business</a></p><div class="clear"></div></li>
+						<li><img src="images/arrow.png" alt=""><p><a href="#">inspiration</a></p><div class="clear"></div></li>
+						<li><img src="images/arrow.png" alt=""><p><a href="#">marketing</a></p><div class="clear"></div></li>
+					</ul>
+					<ul class="blog-list">
+						<li><img src="images/arrow.png" alt=""><p><a href="#">html</a></p><div class="clear"></div></li>
+						<li><img src="images/arrow.png" alt=""><p><a href="#">css</a></p><div class="clear"></div></li>
+						<li><img src="images/arrow.png" alt=""><p><a href="#">news</a></p><div class="clear"></div></li>
+						<li><img src="images/arrow.png" alt=""><p><a href="#">mobile</a></p><div class="clear"></div></li>
+						<li><img src="images/arrow.png" alt=""><p><a href="#">usability</a></p><div class="clear"></div></li>
+					</ul>
+				</div>	
+
+				<div class="popular-posts">
+			     	<h4>Popular Posts</h4>
+					<ul class="blog-list">
+						<li><img src="images/arrow.png" alt=""><p><a href="#">October  (7)</a></p><div class="clear"></div></li>
+						<li><img src="images/arrow.png" alt=""><p><a href="#">September  (9)</a></p><div class="clear"></div></li>
+						<li><img src="images/arrow.png" alt=""><p><a href="#">August  (12)</a></p><div class="clear"></div></li>
+						<li><img src="images/arrow.png" alt=""><p><a href="#">July  (17)</a></p><div class="clear"></div></li>
+						<li><img src="images/arrow.png" alt=""><p><a href="#">June  (6)</a></p><div class="clear"></div></li>
+					</ul>
+					<ul class="blog-list">
+						<li><img src="images/arrow.png" alt=""><p><a href="#">May  (15)</a></p><div class="clear"></div></li>
+						<li><img src="images/arrow.png" alt=""><p><a href="#">April  (4)</a></p><div class="clear"></div></li>
+						<li><img src="images/arrow.png" alt=""><p><a href="#">March  (13)</a></p><div class="clear"></div></li>
+						<li><img src="images/arrow.png" alt=""><p><a href="#">February  (19)</a></p><div class="clear"></div></li>
+						<li><img src="images/arrow.png" alt=""><p><a href="#">January  (21)</a></p><div class="clear"></div></li>
+					</ul>
+				</div>		 
+			</div>
+		</div>
+
+		<ul class="pagination">
+		    <li><a href="#" class="prev">prev</a></li>
+		    <li><a href="#">1</a></li>
+		    <li><a href="#">2</a></li>
+		    <li><a href="#" class="current">3</a></li>
+		    <li><a href="#">4</a></li>
+		    <li><a href="#">5</a></li>
+		    <li><a href="#">...</a></li>
+		    <li><a href="#">19</a></li>
+		    <li><a href="#">20</a></li>
+		    <li><a href="#" class="next">next</a></li>
+	   	</ul>		
+
+        <footer class='wrapper-full'>
+            <div class='limited-content'>
+                <div class='inside-space'>
+                    <div class='footer-contacts-all'>
+                        <div class='footer-column' id='first-column'>
+                            <h5>About Us</h5>
+                            <p class='small first-column'>
+                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                                sed diam nonummy nibh euismod tincidunt ut
+                                laoreet dolore magna aliquam erat volutpat. Ut wisi
+                                enim ad minim veniam, quis nostrud exerci tation ullamcorper
+                                suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+                            </p>
+                            <br>
+                            <a href='#'>Learn More</a>
+                            <h5>Photo Stream</h5>
+                            <img alt='img' src='images/img1.jpg'>
+                            <img alt='img' src='images/img2.jpg'>
+                            <img alt='img' src='images/img3.jpg'>
+                            <img alt='img' src='images/img4.jpg'>
+                            <img alt='img' src='images/img5.jpg'>
+                            <img alt='img' src='images/img6.jpg'>
+                        </div>
+                        <div class='footer-column' id='second-column'>
+                            <h5>Latest tweets</h5>
+                            <div class='footer-tweet'>
+                                <img alt='Twitter birds icon' src='images/twitter_birds_icon.png'>
+                                <p class='small'>Check Out Dtbaker's
+                                    <span class='footer-yellow'>@Arduino</span>
+                                    Sales Notification
+                                    <span class='footer-yellow'>&#35;System</span>
+                                    <a href='#'>http://t.co/WBFKIWHJ</a>
+                                <aside>3 days ago</aside>
+                                </p>
+                            </div>
+                            <div class='footer-tweet'>
+                                <img alt='Twitter birds icon' src='images/twitter_birds_icon.png'>
+                                <p class='small'>Community Superstar Winner РІР
+                                    <span class='footer-yellow'>&#35;OrganicBeeMedia</span>
+                                    <a href='#'>http://t.co/2Fs1nnMj</a>
+                                <aside>3 days ago</aside>
+                                </p>
+                            </div>
+                            <div class='footer-tweet'>
+                                <img alt='Twitter birds icon' src='images/twitter_birds_icon.png'>
+                                <p class='small'>Clean
+                                    <span class='footer-yellow'>&#35;Websites</span>
+                                    Designs for Your
+                                    <span class='footer-yellow'>&#35;Inspiration</span>
+                                    <a href='#'>http://t.co/SnRKu3HJ</a>
+                                <aside>5 days ago</aside>
+                                </p>
+                            </div>
+                            <div class='footer-tweet'>
+                                <img alt='Twitter birds icon' src='images/twitter_birds_icon.png'>
+                                <p class='small'>Cute Online
+                                    <span class='footer-yellow'>&#35;Shops</span>
+                                    <a href='#'>http://t.co/1PoQN3bJ</a>
+                                <aside>6 days ago</aside>
+                                </p>
+                            </div>
+                            <div class='footer-tweet'>
+                                <img alt='Twitter birds icon' src='images/twitter_birds_icon.png'>
+                                <p class='small'>Download 40 vector
+                                    <span class='footer-yellow'>icons</span>
+                                    package for
+                                    <span class='footer-yellow'>&#35;FREE</span>
+                                    <a href='#'>http://t.co/2Fp5GLqn</a>
+                                <aside>6 days ago</aside>
+                                </p>
+                            </div>
+                            <h5>Social Connecting
+	                            <div class='footer-social'>
+	                                <a href='#'><img alt='facebook' src='images/facebook.png'></a>
+	                                <a href='#'><img alt='google_plus' src='images/google_plus.png'></a>
+	                                <a href='#'><img alt='twitter' src='images/twitter.png'></a>
+	                                <a href='#'><img alt='linkedin' src='images/linkedin.png'></a>
+	                                <a href='#'><img alt='you_tube' src='images/youtube.png'></a>
+	                                <a href='#'><img alt='bloger' src='images/bloger.png'></a>
+	                                <a href='#'><img alt='rss' src='images/rss.png'></a>
+	                                <a href='#'><img alt='dribble' src='images/dribble.png'></a>
+	                            </div>
+                            </h5>
+                        </div>
+                        <div class='footer-column' id='third-column'>
+                            <h5>Contact info</h5>
+                        
+                        <div>
+                            <img alt='adress' src='images/address.png'>
+                            <p class='small'>
+                                Address: 12569 Saint Patrick des Pres,
+                                <br>
+                                85000 Paris, France
+                            </p>
+                        </div>
+                        <div>
+                            <img alt='phone' src='images/phone.png'>
+                            <p class='small'>
+                                Phone:
+                            <p class='small'>
+                                +38 045 845-45-78
+                                <br>
+                                +38 045 845-45-79
+                            </p>
+                            </p>
+                        </div>
+                        <div>
+                            <img alt='email' src='images/email.png'>
+                            <p class="small">
+                                E-mail:
+                                <a href='#'>freeforwebdesign@gmail.com</a>
+                            </p>
+                        </div>
+                        <h5>Follow Us</h5>
+                        <p class='small'>
+                            Lorem Ipsum is simply dummy text of the printing
+                            and typesetting industry.
+                        </p>
+                        <form>
+                            <input class='footer-follow-us'>
+                            <a href='#'>
+                            <img alt='email_icon' src='images/email_icon.png'>
+                            </a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+	        <div class='footer-bottom'>
+	            <div class='limited-content'>
+	                <div class='inside-space'>
+	                    <p class='small'>
+	                    Copyright 2013 - FreeForWebDesign.com - All Rights Reserved
+	                    </p>
+	                    <nav>
+	                        <ul class='footer-menu-button'>
+	                            <li><a href="#">home</a></li>
+	                            <li><a href="#">portfolio</a></li>
+	                            <li><a href="#">blog</a></li>
+	                            <li><a href="#">about us</a></li>
+	                            <li><a href="#">contact</a></li>
+	                        </ul>
+	                    </nav>
+	                </div>
+	            </div>
+	        </div>
+	    </footer>
+
+	</body>
+</html>
